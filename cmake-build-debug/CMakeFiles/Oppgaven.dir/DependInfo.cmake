55afbc0bc2984128328fafb76980f8ec58c02428
Clement Marescaux <marcle15@student.westerdals.no>
2017-04-03 12:44:50 +0200

    Push project template

---
+++ cmake-build-debug/CMakeFiles/Oppgaven.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/GameManager.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/GameManager.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/InputManager.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/InputManager.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/Main.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/Main.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/SDL/SDLBmp.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLBmp.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/SDL/SDLError.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLError.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/SDL/SDLManager.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLManager.cpp.o"
+  "/cygdrive/c/Users/Clément/Downloads/Oppgaven/SDL2_Standardproject/Timer.cpp" "/cygdrive/c/Users/Clément/Downloads/Oppgaven/cmake-build-debug/CMakeFiles/Oppgaven.dir/SDL2_Standardproject/Timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Oppgaven.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLBmp.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLError.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/SDL/SDLManager.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/GameManager.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/InputManager.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/Main.cpp.o"
+  "CMakeFiles/Oppgaven.dir/SDL2_Standardproject/Timer.cpp.o"
+  "Oppgaven.pdb"
+  "Oppgaven.exe"
+  "libOppgaven.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oppgaven.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
