62bc43ef0de345ecaa3f865e0c8ede9c09a614f6
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-06-01 14:36:23 +0200

    Add verbose messages

--- travis.sh
+++ travis.sh
@@ -7,7 +7,9 @@ sudo mkdir cmake-build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles"
 
 # run tests
+echo "\n\n\nBuilding and running tests"
 cmake --build . --target runSnakePlusPlusTests
 
 # run program
+echo "\n\n\nBuilding and running executable"
 cmake --build . --target snake-plusplus_run
