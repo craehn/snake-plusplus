34c259102c62523c4a6ec1670e5a546017fde125
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2017-06-01 13:57:21 +0200

    Add cmake path, try running executables

--- travis.sh
+++ travis.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 echo ${PATH}
+
+echo "CMake path:"
+which cmake
+
+# create cmake directory
+cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles"
+
+# run tests
+cmake --build cmake-build-debug --target runSnakePlusPlusTests
+
+# run program
+cmake --build cmake-build-debug --target runSnakePlusPlusTests
